8b88b3a635de8e3fd08a55d1927f0ade8d1a32de
1jestor1 <jtorres122436@gmail.com>
2022-08-25 16:13:22 -0500

    created spring boot

---
+++ rpg-sheet-api/src/main/java/com/rpgsheets/RpgSheetApiApplication.java
@@ -0,0 +1,13 @@
+package com.rpgsheets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgSheetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgSheetApiApplication.class, args);
+	}
+
+}
---
+++ rpg-sheet-api/src/test/java/com/rpgsheets/RpgSheetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpgsheets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgSheetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
