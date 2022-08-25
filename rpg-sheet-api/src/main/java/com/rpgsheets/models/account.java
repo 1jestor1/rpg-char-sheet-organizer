0d118f46a40abcba96ba0964675f3f85b407bda9
1jestor1 <jtorres122436@gmail.com>
2022-08-25 17:01:33 -0500

    created account model

---
+++ rpg-sheet-api/src/main/java/com/rpgsheets/models/account.java
@@ -0,0 +1,25 @@
+package com.rpgsheets.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="rpgaccounts")
+public class account {
+
+	@Id
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="username")
+	private String username;
+	
+	@Column(name="password")
+	private String password;
+	
+}
